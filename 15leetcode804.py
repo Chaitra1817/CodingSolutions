f3ac62a7e55be0cccf69fcdd1b6513f48f0ee429
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2021-05-26 21:23:43 +0530

    Create 15leetcode804.py

---
+++ 15leetcode804.py
@@ -0,0 +1,51 @@
+'''
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+
+'''
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        d={"a":".-","b":"-...","c":"-.-.","d":"-..","e":".","f":"..-.","g":"--.","h":"....","i":"..","j":".---","k":"-.-","l":".-..","m":"--","n":"-.","o":"---","p":".--.","q":"--.-","r":".-.","s":"...","t":"-","u":"..-","v":"...-","w":".--","x":"-..-","y":"-.--","z":"--.."}
+        
+        str1=''
+        set1=set()
+        for i in words:
+            for j in i:
+                str1+=d[j]
+            set1.add(str1)
+            str1=''
+        print(set1) 
+        return(len(set1))
+            
+        
+        #or
+    
+        
+        m = {"a":".-","b":"-...","c":"-.-.","d":"-..","e":".","f":"..-.","g":"--.","h":"....","i":"..","j":".---","k":"-.-","l":".-..","m":"--","n":"-.","o":"---","p":".--.","q":"--.-","r":".-.","s":"...","t":"-","u":"..-","v":"...-","w":".--","x":"-..-","y":"-.--","z":"--.."}
+        s = ''
+        l = list()
+        for i in words:
+            for c in i:
+                s += m[c]
+            l.append(s)
+            s = ''
+        return len(set(l))
+        
