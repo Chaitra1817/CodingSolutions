d525ef92f6354e65cf4c94827681e54c7b975f72
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2023-11-14 22:21:26 +0530

    Create 1930.PalindromicSubsequences.py

---
+++ 1930.PalindromicSubsequences.py
@@ -0,0 +1,58 @@
+'''
+1930. Unique Length-3 Palindromic Subsequences
+Medium
+1.4K
+56
+
+Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
+
+Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
+
+A palindrome is a string that reads the same forwards and backwards.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+    For example, "ace" is a subsequence of "abcde".
+
+ 
+
+Example 1:
+
+Input: s = "aabca"
+Output: 3
+Explanation: The 3 palindromic subsequences of length 3 are:
+- "aba" (subsequence of "aabca")
+- "aaa" (subsequence of "aabca")
+- "aca" (subsequence of "aabca")
+
+Example 2:
+
+Input: s = "adc"
+Output: 0
+Explanation: There are no palindromic subsequences of length 3 in "adc".
+
+Example 3:
+
+Input: s = "bbcbaba"
+Output: 4
+Explanation: The 4 palindromic subsequences of length 3 are:
+- "bbb" (subsequence of "bbcbaba")
+- "bcb" (subsequence of "bbcbaba")
+- "bab" (subsequence of "bbcbaba")
+- "aba" (subsequence of "bbcbaba")
+'''
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        letters = set(s)
+        ans = 0
+        
+        for letter in letters:
+            i, j = s.index(letter), s.rindex(letter)
+            between = set()
+            
+            for k in range(i + 1, j):
+                between.add(s[k])
+            ans += len(between)
+
+        return ans
