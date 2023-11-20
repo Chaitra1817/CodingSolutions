0a98eae94f9d5b46d52f11ec2f2ba86868beed94
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2023-11-20 14:59:01 +0530

    Create 1101.EarliestMomentWhenEveryoneBecomesFriends.py

---
+++ 1101.EarliestMomentWhenEveryoneBecomesFriends.py
@@ -0,0 +1,68 @@
+'''
+Description
+Editorial
+Editorial
+Solutions
+Solutions
+Submissions
+Submissions
+Accepted
+Accepted
+Code
+Testcase
+Testcase
+Test Result
+1101. The Earliest Moment When Everyone Become Friends
+Solved
+Medium
+Topics
+Companies
+Hint
+
+There are n people in a social group labeled from 0 to n - 1. You are given an array logs where logs[i] = [timestampi, xi, yi] indicates that xi and yi will be friends at the time timestampi.
+
+Friendship is symmetric. That means if a is friends with b, then b is friends with a. Also, person a is acquainted with a person b if a is friends with b, or a is a friend of someone acquainted with b.
+
+Return the earliest time for which every person became acquainted with every other person. If there is no such earliest time, return -1.
+
+ 
+
+Example 1:
+
+Input: logs = [[20190101,0,1],[20190104,3,4],[20190107,2,3],[20190211,1,5],[20190224,2,4],[20190301,0,3],[20190312,1,2],[20190322,4,5]], n = 6
+Output: 20190301
+Explanation: 
+The first event occurs at timestamp = 20190101, and after 0 and 1 become friends, we have the following friendship groups [0,1], [2], [3], [4], [5].
+The second event occurs at timestamp = 20190104, and after 3 and 4 become friends, we have the following friendship groups [0,1], [2], [3,4], [5].
+The third event occurs at timestamp = 20190107, and after 2 and 3 become friends, we have the following friendship groups [0,1], [2,3,4], [5].
+The fourth event occurs at timestamp = 20190211, and after 1 and 5 become friends, we have the following friendship groups [0,1,5], [2,3,4].
+The fifth event occurs at timestamp = 20190224, and as 2 and 4 are already friends, nothing happens.
+The sixth event occurs at timestamp = 20190301, and after 0 and 3 become friends, we all become friends.
+
+Example 2:
+
+Input: logs = [[0,2,0],[1,0,1],[3,0,3],[4,1,2],[7,3,1]], n = 4
+Output: 3
+Explanation: At timestamp = 3, all the persons (i.e., 0, 1, 2, and 3) become friends.
+
+'''
+
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+        logs=sorted(logs,key=lambda x:x[0])
+        graph=defaultdict(set)
+
+        for i in range(n):
+            graph[i]={i}
+        
+        for t,f1,f2 in logs:
+            graph[f1]=graph[f1].union(graph[f2]) 
+
+            for f3 in graph[f1]:
+                graph[f3]=graph[f1]
+            
+            if len(graph[f1])==n:
+                return t
+
+        return -1
+
