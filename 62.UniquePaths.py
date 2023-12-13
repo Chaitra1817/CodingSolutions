28cc9157ecc31317d4cc22f8e450181135a95aa0
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2023-12-13 14:54:44 +0530

    Update 62.UniquePaths.py

--- 62.UniquePaths.py
+++ 62.UniquePaths.py
@@ -27,7 +27,7 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 3. Down -> Right -> Down
 
 '''
-
+# Memoization
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp=[[-1 for i in range(n)] for j in range(m)]
@@ -49,3 +49,25 @@ class Solution:
             return dp[row][col]
         
         return findPaths(m-1,n-1)
+
+
+
+        
+# Tabulation
+        dp=[[-1 for i in range(n)]for j in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if i==0 and j==0:
+                    dp[i][j]=1
+                else:
+                    up=0
+                    left=0
+                    if i>0:
+                        up=dp[i-1][j]
+                    if j>0:
+                        left=dp[i][j-1]
+                    
+                    dp[i][j]=up+left
+        
+        return dp[m-1][n-1]
