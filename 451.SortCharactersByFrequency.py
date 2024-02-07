34b45fc49c226e3d64b460122e6b0825bb9b4d2d
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2024-02-07 09:28:26 +0530

    Create 451.SortCharactersByFrequency.py

---
+++ 451.SortCharactersByFrequency.py
@@ -0,0 +1,42 @@
+'''
+Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+ 
+
+Example 1:
+
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+Example 2:
+
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+
+Example 3:
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+
+
+ '''
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        d=defaultdict(int)
+        for i in s:
+            d[i]+=1
+        
+        sd=dict(sorted(d.items(),key=lambda item:item[1]))
+        ans=""
+        for k,v in sd.items():
+            ans+=k*v
+        return ans[::-1]
