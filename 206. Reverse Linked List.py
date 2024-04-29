62af31b43d4a977cd5db3d64977e54c19e8bcf29
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2024-04-29 22:34:42 +0530

    Create 206. Reverse Linked List.py

---
+++ 206. Reverse Linked List.py
@@ -0,0 +1,29 @@
+'''
+206. Reverse Linked List
+Solved
+Easy
+Topics
+Companies
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+'''
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+            
+        return prev
