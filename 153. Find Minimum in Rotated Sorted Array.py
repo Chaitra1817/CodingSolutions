1df726e0a54ad109b3813e2a9d4bc4555a38203e
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2024-06-29 16:04:21 +0530

    Create 153. Find Minimum in Rotated Sorted Array.py

---
+++ 153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,60 @@
+'''
+153. Find Minimum in Rotated Sorted Array
+Solved
+Medium
+Topics
+Companies
+Hint
+
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+    [4,5,6,7,0,1,2] if it was rotated 4 times.
+    [0,1,2,4,5,6,7] if it was rotated 7 times.
+
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+
+'''
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        n=len(nums)
+        low,high=0,n-1
+        mn=float('inf')
+        while low<=high:
+            mid=(low+high)//2
+            if nums[low]<=nums[high]:
+                mn=min(mn,nums[low])
+                break
+            if nums[low]<=nums[mid]:
+                mn=min(mn,nums[low])
+                low=mid+1
+            else:
+                mn=min(mn,nums[mid])
+                high=mid-1
+        
+        return mn
+
