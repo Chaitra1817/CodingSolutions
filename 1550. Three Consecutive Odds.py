fc3da30d7594f6e73a1ea9d8ebc58f6a81ead8e3
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2024-07-01 08:39:22 +0530

    Create 1550. Three Consecutive Odds.py

---
+++ 1550. Three Consecutive Odds.py
@@ -0,0 +1,38 @@
+'''
+1550. Three Consecutive Odds
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+
+'''
+
+from typing import List
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        cnt = 0
+        for i in range(len(arr)):
+            if arr[i] & 1:  # Check if the number is odd
+                cnt += 1
+                if cnt == 3:
+                    return True
+            else:
+                cnt = 0
+        return False
