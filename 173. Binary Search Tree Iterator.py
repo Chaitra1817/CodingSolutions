45222783684e141fed12ed2e0db4b1b0e23ed1c1
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2025-05-01 23:18:01 +0530

    Create 173. Binary Search Tree Iterator.py

---
+++ 173. Binary Search Tree Iterator.py
@@ -0,0 +1,55 @@
+'''
+
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+    BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+    boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+    int next() Moves the pointer to the right, then returns the number at the pointer.
+
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+ 
+
+Example 1:
+
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack=[]
+        self.leftInorder(root)
+    
+    def leftInorder(self,node):
+        while node:
+            self.stack.append(node)
+            node=node.left 
+
+    def next(self) -> int:
+        top=self.stack.pop()
+        if top.right:
+            self.leftInorder(top.right)
+        return top.val
+
+
+    def hasNext(self) -> bool:
+        return len(self.stack)>0
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+'''
+
