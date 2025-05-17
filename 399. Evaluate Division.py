1a67a3d5744870dbbdf27b55b47f9b4e57051d60
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2025-05-17 15:50:18 +0530

    Create 399. Evaluate Division.py

---
+++ 399. Evaluate Division.py
@@ -0,0 +1,88 @@
+'''
+399. Evaluate Division
+Solved
+Medium
+Topics
+Companies
+Hint
+
+You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+
+You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+
+Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+
+Note: The variables that do not occur in the list of equations are undefined, so the answer cannot be determined for them.
+
+ 
+
+Example 1:
+
+Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+Explanation: 
+Given: a / b = 2.0, b / c = 3.0
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? 
+return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+note: x is undefined => -1.0
+
+Example 2:
+
+Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+Output: [3.75000,0.40000,5.00000,0.20000]
+
+Example 3:
+
+Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+Output: [0.50000,2.00000,-1.00000,-1.00000]
+
+ 
+
+Constraints:
+
+    1 <= equations.length <= 20
+    equations[i].length == 2
+    1 <= Ai.length, Bi.length <= 5
+    values.length == equations.length
+    0.0 < values[i] <= 20.0
+    1 <= queries.length <= 20
+    queries[i].length == 2
+    1 <= Cj.length, Dj.length <= 5
+    Ai, Bi, Cj, Dj consist of lower case English letters and digits.
+
+
+'''
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj=defaultdict(list)
+        for i,eq in enumerate(equations):
+            u,v=eq
+            adj[u].append((v,values[i]))
+            adj[v].append((u,1/values[i]))
+        
+        def bfs(s,d):
+            if s not in adj or d not in adj:
+                return -1
+            q=deque([])
+            q.append((s,1))
+            visited=set()
+
+            while q:
+                node,weight =q.popleft()
+                
+                for neigh,w in adj[node]:
+                    if neigh==d:
+                        return weight*w
+                    elif neigh not in visited:
+                        visited.add(neigh)
+                        q.append((neigh,weight*w))
+            return -1
+        
+        
+        return[bfs(s,d) for s,d in queries]
+
+
+         
