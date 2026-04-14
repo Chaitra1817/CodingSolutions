543c9d724cd00352a913b904528e36d279f482f6
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2026-04-14 09:55:40 +0530

    Create 2435. Paths in Matrix Whose Sum Is Divisible by K.py

---
+++ 2435. Paths in Matrix Whose Sum Is Divisible by K.py
@@ -0,0 +1,99 @@
+'''
+2435. Paths in Matrix Whose Sum Is Divisible by K
+Solved
+Hard
+
+Topics
+premium lock icon
+Companies
+
+Hint
+You are given a 0-indexed m x n integer matrix grid and an integer k. You are currently at position (0, 0) and you want to reach position (m - 1, n - 1) moving only down or right.
+
+Return the number of paths where the sum of the elements on the path is divisible by k. Since the answer may be very large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[5,2,4],[3,0,5],[0,7,2]], k = 3
+Output: 2
+Explanation: There are two paths where the sum of the elements on the path is divisible by k.
+The first path highlighted in red has a sum of 5 + 2 + 4 + 5 + 2 = 18 which is divisible by 3.
+The second path highlighted in blue has a sum of 5 + 3 + 0 + 5 + 2 = 15 which is divisible by 3.
+Example 2:
+
+
+Input: grid = [[0,0]], k = 5
+Output: 1
+Explanation: The path highlighted in red has a sum of 0 + 0 = 0 which is divisible by 5.
+Example 3:
+
+
+Input: grid = [[7,3,4,9],[2,3,6,2],[2,3,7,0]], k = 1
+Output: 10
+Explanation: Every integer is divisible by 1 so the sum of the elements on every possible path is divisible by k.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 5 * 104
+1 <= m * n <= 5 * 104
+0 <= grid[i][j] <= 100
+1 <= k <= 50
+
+'''
+
+class Solution:
+    def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        dp=[[[0]*k for i in range(n)] for j in range(m)]
+
+        MOD=10**9+7
+        dp[0][0][grid[0][0] % k] = 1
+
+        for i in range(m):
+            for j in range(n):
+                for r in range(k):
+                    ways=dp[i][j][r]
+                    if ways == 0:
+                        continue
+
+                    if j+1<=n-1:
+                        nr=(r+grid[i][j+1])%k
+                        dp[i][j+1][nr]=(dp[i][j+1][nr]+ways)%MOD
+                    if i+1<=m-1:
+                        nr=(r+grid[i+1][j])%k
+                        dp[i+1][j][nr]=(dp[i+1][j][nr]+ways)%MOD
+                    
+        return dp[m-1][n-1][0]
+
+
+
+'''
+class Solution:
+    def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        MOD = 10**9 + 7
+        memo = {}
+
+        def find(i, j, r):
+            if i >= m or j >= n:
+                return 0
+
+            r = (r + grid[i][j]) % k
+
+            if i == m - 1 and j == n - 1:
+                return 1 if r == 0 else 0
+
+            if (i, j, r) in memo:
+                return memo[(i, j, r)]
+
+            memo[(i, j, r)] = (find(i, j + 1, r) + find(i + 1, j, r)) % MOD
+            return memo[(i, j, r)]
+
+        return find(0, 0, 0)
+'''
