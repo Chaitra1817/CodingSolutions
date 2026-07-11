a6dba7460705dd71b5f6597f254e0fce406224eb
Chaitra1817 <49087033+Chaitra1817@users.noreply.github.com>
2026-07-11 13:54:52 +0530

    Create   Code  Testcase Testcase  Test Result 312. Burst Balloons.py

---
+++ Code  Testcase Testcase  Test Result 312. Burst Balloons.py
@@ -0,0 +1,50 @@
+'''
+You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+
+Return the maximum coins you can collect by bursting the balloons wisely.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+Example 2:
+
+Input: nums = [1,5]
+Output: 10
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+0 <= nums[i] <= 100
+'''
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums.insert(0,1)
+        nums.append(1)
+        memo={}
+        def find(i,j):
+            if i>j:
+                return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            
+            ans=0
+            for idx in range(i,j+1):
+                cur=nums[i-1]*nums[idx]*nums[j+1]+find(i,idx-1)+find(idx+1,j)
+                ans=max(ans,cur)
+
+            memo[(i,j)] = ans
+            return memo[(i,j)]
+
+        return find(1,len(nums)-2)
+        
